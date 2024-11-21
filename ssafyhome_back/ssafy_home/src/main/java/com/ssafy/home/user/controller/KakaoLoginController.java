38327935ae207550dd84672d571617025cebda5c
박준현 <wndrjrak12@gmail.com>
2024-11-21 09:05:14 +0900

    merge전 push

--- ssafyhome_back/ssafy_home/src/main/java/com/ssafy/home/user/controller/KakaoLoginController.java
+++ ssafyhome_back/ssafy_home/src/main/java/com/ssafy/home/user/controller/KakaoLoginController.java
@@ -1,5 +1,6 @@
 package com.ssafy.home.user.controller;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,39 +9,25 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.ssafy.home.user.model.AuthResponse;
-import com.ssafy.home.user.model.service.KakaoLoginService;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @RestController
 @RequestMapping("/oauth/kakao")
+@Slf4j
 public class KakaoLoginController {
 
-	
-
-//    @GetMapping("/callback")
-//    public ResponseEntity<AuthResponse> kakaoCallback() {
-//    	System.out.println("ghkrdls");
-//		return null;
-//    }
-	
-//    @Value("${kakao.client.id}")
-//    private String clientId;
-//
-//    @Value("${kakao.redirect.uri}")
-//    private String redirectUri;
-//
-//    private final KakaoLoginService kakaoLoginService;
-//
-//    @Autowired
-//    public KakaoLoginController(KakaoLoginService kakaoLoginService) {
-//        this.kakaoLoginService = kakaoLoginService;
-//    }
 
     @GetMapping("/callback")
-    public ResponseEntity<?> kakaoLogin(@RequestParam("code") String code ) {
+    public ResponseEntity<?> kakaoLogin(@RequestParam String code , @RequestParam(required = false) String error) {
     	System.out.println(code);
+    	
     	return null;
     }
+    
+    
 }
