a4ac88c46c95974023c36f23ff12c3eea14e9aed
박준현 <wndrjrak12@gmail.com>
2024-11-22 11:41:03 +0900

    임시

--- ssafyhome_front/src/main.js
+++ ssafyhome_front/src/main.js
@@ -2,10 +2,16 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 
 import "@/assets/main.css";
 
+
+const pinia = createPinia();
 const app = createApp(App);
-app.use(createPinia());
+pinia.use(piniaPluginPersistedstate);
+
+app.use(pinia);
 app.use(router);
+
 app.mount("#app");
